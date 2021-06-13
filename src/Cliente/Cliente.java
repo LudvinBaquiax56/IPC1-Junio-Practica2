6fcea0f013ea6f583e6c0d6f61ca4445c3208f0b
LudvinBaquiax56 <vasqueznicolas56@gmail.com>
2021-06-12 18:19:13 -0600

    Objeto cliente creado

---
+++ src/Cliente/Cliente.java
@@ -0,0 +1,56 @@
+package src.Cliente;
+
+public class Cliente {
+
+	private String nombre;
+	private int id;
+	private int telefono;
+	private boolean tienePrestado;
+
+	public Cliente (String nombre, int id, int telefono, boolean tienePrestado){
+		this.nombre = nombre;
+		this.id = id;
+		this.telefono = telefono;
+		this.tienePrestado = tienePrestado; 
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getTelefono(){
+		return telefono;
+	}
+
+	public void setTelefono(int telefono){
+		this.telefono = telefono;
+	}
+
+	public boolean getTienePrestado(){
+		return tienePrestado;
+	}
+
+	public void setTienePrestado(boolean tienePrestado){
+		this.tienePrestado = tienePrestado;
+	}
+
+	public void cambiarEstado(){
+		this.tienePrestado = (!tienePrestado);
+	}
+
+	public String mostrarDatos(){
+		return nombre + "--" + id + "--" + telefono + "--" + tienePrestado;
+	}
+}
--- src/Inicio.java
+++ src/Inicio.java
@@ -6,7 +6,7 @@ public class Inicio {
 
     public static void main(String[] args) {
        	Sistema sistema = new Sistema();   
-        sistema.mostrarMenu();    
+        sistema.mostrarMenu();           
     }
     
 }
